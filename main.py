0ca5db4cc4ebd3b55ed7b15f8638714ae321d433
Eric Fugal <ericleefugal@gmail.com>
2026-08-03 11:49:50 -0600

    line clears and game over logic

--- main.py
+++ main.py
@@ -36,12 +36,16 @@ def main():
     hasActivePiece = False
     level = 0
     frames_to_fall_table = [30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12]
+    line_cleared = False
+    changes_made = False
+    fps = 60
 
     running = True
     while running: # Main game loop
-        window.fill((0,0,0))
+        window.fill((128,128,128))
         
         if mode == "game":
+            fps = 30
             dir_to_move = (0,0)
             hasRotated = False
             if not hasActivePiece:
@@ -103,20 +107,64 @@ def main():
                         pos = (anchor[0]+offset[0], anchor[1]+offset[1])
                         board[pos[1]][pos[0]] = 1
                     hasActivePiece = False
-                    # TODO: check for line clears
-                    # TODO: check for death
+                    for row_num in range(len(board)):
+                        row = board[row_num]
+                        row_is_full = True
+                        for tile in row:
+                            row_is_full = tile != 0 and row_is_full
+                        if row_is_full:
+                            mode = "line clear"
+                    if anchor[1] > 20:
+                        mode = "game over"
+                        height = 22
             else:
                 frames_to_fall -= 1
-            
-            # draw to the screen
-            for row_num in range(len(board)):
-                for col_num in range(len(board[row_num])):
-                    pygame.draw.rect(window, (0, 0, 255*board[row_num][col_num]), (440+col_num*32, 738-row_num*32, 32, 32))
+
+        elif mode == "line clear":
+            fps = 10
+            if line_cleared or changes_made:
+                changes_made = False
+                for row_num in range(20):
+                    row = board[row_num]
+                    next_row = board[row_num+1]
+                    for tile_num in range(len(row)):
+                        if row[tile_num] == 0 and next_row[tile_num] != 0:
+                            board[row_num][tile_num] = next_row[tile_num]
+                            board[row_num+1][tile_num] = 0
+                            changes_made = True
+
+            if not changes_made:
+                line_cleared = False
+                for row_num in range(len(board)):
+                    row = board[row_num]
+                    row_is_full = True
+                    for tile in row:
+                        row_is_full = tile != 0 and row_is_full
+                    if row_is_full:
+                        for tilenum in range(len(row)):
+                            board[row_num][tilenum] = 0
+                        line_cleared = True
+                if not line_cleared:
+                    mode = "game"
+
+        elif mode == "game over":
+            if height >= 0:
+                for tilenum in range(len(board[height])):
+                    board[height][tilenum] = 1
+            height -= 1
+            if height < -10:
+                mode = "line clear"
+        
+        # draw to the screen
+        for row_num in range(len(board)):
+            for col_num in range(len(board[row_num])):
+                pygame.draw.rect(window, (0, 0, 255*board[row_num][col_num]), (440+col_num*32, 738-row_num*32, 32, 32))
+        if mode == "game" and hasActivePiece:
             for offset in offsets:
                 pos = (anchor[0]+offset[0], anchor[1]+offset[1])
                 pygame.draw.rect(window, (0, 0, 255), (440+pos[0]*32, 738-pos[1]*32, 32, 32))
-            pygame.display.flip()
-            clock.tick(30)
+        pygame.display.flip()
+        clock.tick(fps)
 
 if __name__ == "__main__":
     main()
